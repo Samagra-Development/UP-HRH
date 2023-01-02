e2e0b5d0882e16bf33545676c62cace2f8d2840b
Shruti3004 <mail2shruti.ag@gmail.com>
2023-01-02 14:48:40 +0530

    fix: maps
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/pages/CaptureLocation.jsx
+++ src/pages/CaptureLocation.jsx
@@ -3,31 +3,43 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import CommonLayout from "../components/CommonLayout";
 
 const CaptureLocation = () => {
-    const [lat, setLat] = useState(0);
-    const [long, setLong] = useState(0);
-    const [showMap, setShowMap] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const [showMap, setShowMap] = useState(false);
 
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((p) => { setLat(p.coords.latitude); setLong(p.coords.longitude); setShowMap(true) });
-        } else {
-
-        }
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((p) => {
+        setLat(p.coords.latitude);
+        setLong(p.coords.longitude);
+        setShowMap(true);
+      });
+    } else {
     }
-    useEffect(() => {
-        getLocation();
-    }, [])
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-    return (
-        <CommonLayout>
-            <div className="flex flex-col px-5 py-8 items-center">
-                <p>{lat}</p>
-                <p>{long}</p>
-                {/* {lat && long && <iframe src={`https://maps.google.com/maps?q=${lat},${long}&hl=es;z=14&amp;output=embed`}></iframe>}
-                <iframe src={`https://maps.google.com/maps?q=28.5611047,77.1975637&hl=es;z=14&amp;output=embed`}></iframe> */}
-            </div>
-        </CommonLayout >
-    );
+  return (
+    <CommonLayout>
+      <div className="flex flex-col px-5 py-8 items-center">
+        <i class="fa-sharp fa-solid fa-location-dot"></i>
+        <p>{lat}</p>
+        <p>{long}</p>
+        <iframe
+          src={`https://maps.google.com/maps?q=${lat},${long}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+          width="100%"
+          height="380"
+          frameborder="0"
+          allowfullscreen
+          loading="lazy"
+          title="map"
+          className="mt-5"
+        />
+      </div>
+    </CommonLayout>
+  );
 };
 
 export default CaptureLocation;
