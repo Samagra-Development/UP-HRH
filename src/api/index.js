4cd75ccf7a37135eddc9917cbf8111a84325633c
amit-s19 <coutamit.s19@gmail.com>
2023-01-02 08:44:22 +0530

    apis

--- src/api/index.js
+++ src/api/index.js
@@ -5,30 +5,55 @@ const BASE_URL = process.env.REACT_APP_USER_SERVICE_URL;
 const applicationId = process.env.REACT_APP_APPLICATION_ID;
 
 export const loginMedical = async (username, pass) => {
-    try {
-        const res = await axios.post(BASE_URL + "login", {
-            password: pass,
-            loginId: username,
-            applicationId: applicationId
-        });
-        return res.data;
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const res = await axios.post(BASE_URL + "login", {
+      password: pass,
+      loginId: username,
+      applicationId: applicationId
+    });
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 
-export const testHasuraQuery = () => {
-    const query = {
-        query: `
-      query ($iti_id: Int) {
-        test() {
-          batch
-          trade
+export const getMedicalAssessments = () => {
+  const query = {
+    query: `
+      query ($date: date) {
+        institutes(where: {schedule_date: {_eq: $date}}) {
+          name
+          nursing
+          schedule_date
+          paramedical
+          type
+          application_id
+          district
         }
       }
       `,
-        "variables": { iti_id: 9 }
-    };
-    return makeHasuraCalls(query);
+    "variables": { date: new Date().toISOString().split('T')[0] }
+  };
+  return makeHasuraCalls(query);
+};
+
+export const getMedicalAssessmentsUpcoming = () => {
+  const query = {
+    query: `
+      query {
+        institutes(order_by: {schedule_date: asc}){
+          name
+          nursing
+          schedule_date
+          paramedical
+          type
+          application_id
+          district
+        }
+      }
+      `,
+    variables: {}
+  };
+  return makeHasuraCalls(query);
 };
