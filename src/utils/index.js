65b3b706253b17b6a6a18c59fc9b95146bfa7875
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2023-02-21 18:10:21 +0530

    generating user cookie

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,42 +1,60 @@
 export const makeHasuraCalls = async (query) => {
-    const userData = JSON.parse(localStorage.getItem("userData"));
-    return fetch(process.env.REACT_APP_HASURA_URL, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userData.token}`,
-        },
-        body: JSON.stringify(query)
-    }).then(async (response) => await validateResponse(response))
-        .catch((error) => { return error });
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  return fetch(process.env.REACT_APP_HASURA_URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${userData.token}`,
+    },
+    body: JSON.stringify(query),
+  })
+    .then(async (response) => await validateResponse(response))
+    .catch((error) => {
+      return error;
+    });
 };
 
 const validateResponse = async (response) => {
-    const apiRes = await response.json();
-    const jsonResponse = {
-        ...apiRes,
-        responseStatus: false,
-    };
-    return jsonResponse;
+  const apiRes = await response.json();
+  const jsonResponse = {
+    ...apiRes,
+    responseStatus: false,
+  };
+  return jsonResponse;
 };
 
 export const makeDataForPrefill = (prev, xmlDoc, key, finalObj, formName) => {
-    if (Array.isArray(xmlDoc) && xmlDoc.length == 0 && prev.value) {
-        finalObj[key] = prev.value;
-    } else {
-        for (const el in xmlDoc) {
-            makeDataForPrefill(xmlDoc[el], xmlDoc[el].children, key + "_*_" + xmlDoc[el].name, finalObj, formName);
-        }
+  if (Array.isArray(xmlDoc) && xmlDoc.length == 0 && prev.value) {
+    finalObj[key] = prev.value;
+  } else {
+    for (const el in xmlDoc) {
+      makeDataForPrefill(
+        xmlDoc[el],
+        xmlDoc[el].children,
+        key + "_*_" + xmlDoc[el].name,
+        finalObj,
+        formName
+      );
     }
-}
+  }
+};
 
 export const updateFormData = (name, data) => {
-    let newData = JSON.stringify(data);
-    let images = localStorage.getItem(name) ? JSON.parse(localStorage.getItem(name)) : null;
-    if (images) {
-        images.forEach(el => newData = newData.replace(el.name, el.url));
-        return newData;
-    }
-    return JSON.stringify(data);
-}
+  let newData = JSON.stringify(data);
+  let images = localStorage.getItem(name)
+    ? JSON.parse(localStorage.getItem(name))
+    : null;
+  if (images) {
+    images.forEach((el) => (newData = newData.replace(el.name, el.url)));
+    return newData;
+  }
+  return JSON.stringify(data);
+};
+
+export const setCookie = (cname, cvalue, minutes) => {
+  var d = new Date();
+  d.setTime(d.getTime() + minutes * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+};
