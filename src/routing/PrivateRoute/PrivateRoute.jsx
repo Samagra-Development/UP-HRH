fe749813ee51433bed3149671882be34bf5b6bde
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2023-02-22 15:36:03 +0530

    made chages

---
+++ src/routing/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { isUserExists } from "../../utils";
+import { StateContext } from "../../App";
+import ROUTE_MAP from "../routeMap";
+import { Navigate } from "react-router";
+
+const PrivateRoute = ({ children, odk }) => {
+  const { state } = useContext(StateContext);
+  const isAuthenticated = isUserExists();
+  // console.log(state);
+
+  // if (odk && isAuthenticated) {
+  //   if (state && state.userData && state.userData.filledForms && !state.userData.filledForms[odk])
+  //     return children;
+  //   else
+  //     return <Navigate to="/" />
+  // }
+  return isAuthenticated ? children : <Navigate to={ROUTE_MAP.login} />;
+};
+
+export default PrivateRoute;
