211cd796a4c1a202f627dff66c94a1c3ae1e1f89
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2023-02-23 11:18:46 +0530

    fixed broken login

--- src/utils/index.js
+++ src/utils/index.js
@@ -66,3 +66,21 @@ export const extractUserFromCookie = () => {
     return false;
   }
 };
+
+export const deleteAllCookies = () => {
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+};
+
+export const logout = () => {
+  localStorage.clear();
+  sessionStorage.clear();
+  deleteAllCookies();
+  window.location = "/";
+};
